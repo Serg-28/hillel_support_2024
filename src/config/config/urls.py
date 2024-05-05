2c120312df372420f10e73676c322dd04fb6ecb8
Serg-28 <sergbondarenko28@gmail.com>
2024-05-05 21:41:17 +0200

    Finished HW_15

--- src/config/config/urls.py
+++ src/config/config/urls.py
@@ -1,27 +1,12 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
-from issues.api import create_my_post_issue, create_random_issue, get_issues
+from issues.api import *  # noqa
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("issues/", get_issues),
-    path("issues/create-random", create_random_issue),
-    path("issues/create-post-issue", create_my_post_issue),
+    path("issues/", get_issues),  # noqa
+    path("issues/create-random", create_random_issue),  # noqa
+    path("issues/post-issue", post_issue),  # noqa
+    path("issues/create-issue", create_issue),  # noqa
+    path("issues/<int:issue_id>", retreive_issue),  # noqa
 ]
