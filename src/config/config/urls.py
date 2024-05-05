7199eff3c367add466a4be204b8b77ecdfb930c2
Serg-28 <sergbondarenko28@gmail.com>
2024-05-05 20:40:48 +0200

    finished HW_

--- src/config/config/urls.py
+++ src/config/config/urls.py
@@ -1,27 +1,16 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
-from issues.api import create_my_post_issue, create_random_issue, get_issues
+from issues.api import (create_issue, create_poderevyanski_issue,
+                        create_random_issue, get_issues,
+                        get_poderevyanski_issue, post_issue, retreive_issue)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("issues/", get_issues),
     path("issues/create-random", create_random_issue),
-    path("issues/create-post-issue", create_my_post_issue),
+    path("issues/oles", create_poderevyanski_issue),
+    path("issues/get-oles", get_poderevyanski_issue),
+    path("issues/post-issue", post_issue),
+    path("issues/create-issue", create_issue),
+    path("issues/<int:issue_id>", retreive_issue),
 ]
--- src/config/issues/api.py
+++ src/config/issues/api.py
@@ -1,34 +1,58 @@
-# Create your views here.
+import csv
 import json
 import random
 import string
 
+from django.http import Http404  # noqa
 from django.http import HttpRequest, JsonResponse
+from django.shortcuts import render  # noqa
+from django.shortcuts import get_object_or_404
 from issues.models import Issue
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-def get_issues(request: HttpRequest) -> JsonResponse:
-    if request.method != "GET":
-        raise Exception("Only GET method is allowed")
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = "__all__"  # выдает все поля из модели
 
-    issues: list[Issue] = Issue.objects.all()
 
-    results: list[dict] = [
-        {
-            "id": issue.id,
-            "title": issue.title,
-            "body": issue.body,
-            "senior_id": issue.senior_id,
-            "junior_id": issue.junior_id,
-        }
-        for issue in issues
-    ]
+@api_view()
+def get_issues(request) -> Response:
 
-    return JsonResponse(data={"results": results})
+    issues = Issue.objects.all()
+    # results = [IssueSerializer(issue) for issue in issues]
+    results = [IssueSerializer(issue).data for issue in issues]
+
+    return Response(data={"results": results})
+
+
+@api_view()
+def retreive_issue(
+    request, issue_id: int
+) -> Response:  # выведение данных по значению Id в запросе в Postman
+    instance = get_object_or_404(Issue, id=issue_id)
+    return Response(data={"results": IssueSerializer(instance).data})
+
+
+@api_view(["POST"])
+def create_issue(request) -> Response:
+    try:
+        payload: dict = json.loads(request.body)
+    except json.decoder.JSONDecodeError:
+        raise Exception("Request body is invalid")
+
+    serializer = IssueSerializer(data=payload)
+    serializer.is_valid(raise_exception=True)
+
+    issue = Issue.objects.create(**serializer.validated_data)
+    return Response(data=IssueSerializer(issue).data)
 
 
 def _random_string(length: int = 10) -> str:
-    return "".join([random.choice(string.ascii_letters) for i in range(length)])  # noqa
+    return "".join(random.choice(string.ascii_letters) for i in range(length))
 
 
 def create_random_issue(request: HttpRequest) -> JsonResponse:
@@ -50,11 +74,64 @@ def create_random_issue(request: HttpRequest) -> JsonResponse:
     return JsonResponse(data=result)
 
 
-def create_my_post_issue(request: HttpRequest) -> JsonResponse:
+def create_poderevyanski_issue(request: HttpRequest) -> JsonResponse:
+    path = "/Users/vladimirsaratovsky/Documents/GitHub/hillel_support_project/Oles.csv"  # noqa
+    data = []
+    result = []
+    num = 1
 
+    with open(path, newline="", encoding="utf-8") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data.append(row)
+
+    for row in data[1:]:
+
+        new_title = row[1] + str(num)
+
+        issue = Issue.objects.create(
+            title=new_title,
+            body=row[0],
+            senior_id=2,
+            junior_id=1,
+        )
+
+        result.append(
+            {
+                "id": issue.id,
+                "title": issue.title,
+                "body": issue.body,
+                "senior_id": issue.senior_id,
+                "junior_id": issue.junior_id,
+            }
+        )
+
+        num += 1
+
+    return JsonResponse(data=result, safe=False)
+
+
+def get_poderevyanski_issue(request: HttpRequest) -> JsonResponse:
+    issues: list[Issue] = Issue.objects.all()
+
+    results: list[dict] = [
+        {
+            "id": issue.id,
+            "title": issue.title,
+            "body": issue.body,
+            "senior_id": issue.senior_id,
+            "junior_id": issue.junior_id,
+        }
+        for issue in issues
+    ]
+
+    return JsonResponse(data={"results": results})
+
+
+def post_issue(request: HttpRequest) -> JsonResponse:
     post_data = json.loads(request.body)
 
-    issue = Issue.objects.create(
+    issues = Issue.objects.create(
         title=post_data.get("title"),
         body=post_data.get("body"),
         senior_id=post_data.get("senior_id"),
@@ -62,11 +139,10 @@ def create_my_post_issue(request: HttpRequest) -> JsonResponse:
     )
 
     result = {
-        "id": issue.id,
-        "title": issue.title,
-        "body": issue.body,
-        "senior_id": issue.senior_id,
-        "junior_id": issue.junior_id,
+        "title": issues.title,
+        "body": issues.body,
+        "senior_id": issues.senior_id,
+        "junior_id": issues.junior_id,
     }
 
     return JsonResponse(data=result)
